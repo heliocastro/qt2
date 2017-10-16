1cdfe5e29c5ffe88e3829e39117cdceafe204e94
Helio Chissini de Castro <helio@kde.org>
2017-10-16 08:15:47 +0200

    - Remove uneccessary forced link libraries to target

--- cmake/Qt2Config.cmake
+++ cmake/Qt2Config.cmake
@@ -4,19 +4,3 @@ include("${CMAKE_CURRENT_LIST_DIR}/Qt2Utils.cmake")
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads)
-
-find_dependencies(PkgConfig)
-pkg_search_module(XFT REQUIRED xft)
-pkg_search_module(FONTCONFIG REQUIRED fontconfig)
-
-find_dependencies(Freetype)
-
-find_dependencies(ZLIB)
-
-find_dependencies(PNG)
-
-find_dependencies(MNG)
-
-find_dependencies(JPEG)
-
-find_dependencies(X11)
